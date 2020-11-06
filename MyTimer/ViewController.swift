7e1b3e903e4286488c966b791002c6a2b03a856d
yumi KB <bon.kyumi@gmail.com>
2020-11-06 18:24:48 +0900

    Add alert modal view

--- MyTimer/ViewController.swift
+++ MyTimer/ViewController.swift
@@ -102,6 +102,18 @@ class ViewController: UIViewController {
             
             // タイマー停止
             timer.invalidate()
+            
+            // アラート画面を作成
+            let alertController = UIAlertController(title: "終了", message: "タイマー終了時間です", preferredStyle: .alert)
+            
+            // ダイアログに表示させるOKボタンを作成
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            // アクションを追加
+            alertController.addAction(defaultAction)
+            
+            // ダイアログの表示
+            present(alertController, animated: true, completion: nil)
         }
     }
     
